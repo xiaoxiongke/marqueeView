0e0c5f235973bce9eac59aae34ef81e606f2576f
xiaoxiongke <sama323223@gmail.com>
2021-02-25 09:12:33 +0800

    Update GKLDHomeMarqueeViewCell.h

--- GKLDHomeMarqueeViewCell.h
+++ GKLDHomeMarqueeViewCell.h
@@ -2,15 +2,15 @@
 //  GKLDHomeMarqueeViewCell.h
 //  GKLD
 //
-//  Created by 向宇涛 on 2021/1/21.
 //  Copyright © 2021 Mac. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+/// TODO: 导入基类头文件,如果没有就用默认的UITableViewCell
 #import "GKLDBaseTableViewCell.h"
 
 NS_ASSUME_NONNULL_BEGIN
-
+/// TODO: 继承你的cell的基类
 @interface GKLDHomeMarqueeViewCell : GKLDBaseTableViewCell
 
 
