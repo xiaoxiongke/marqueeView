7e899ef2a4f62efd838896f172c116c1a3122572
xiaoxiongke <sama323223@gmail.com>
2021-02-25 09:09:06 +0800

    Update GKLDMarqueeStyleView.h

--- GKLDMarqueeStyleView.h
+++ GKLDMarqueeStyleView.h
@@ -2,21 +2,21 @@
 //  GKLDMarqueeStyleView.h
 //  GKLD
 //
-//  Created by 向宇涛 on 2021/1/21.
 //  Copyright © 2021 Mac. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+/// TODO: 模型头文件
 #import "GKLDHomeBlockListModel.h"
 
 NS_ASSUME_NONNULL_BEGIN
-
+/// 这是自定义的文字广告样式cell
 @interface GKLDMarqueeStyleView : UICollectionViewCell
 
 @property (nonatomic,strong) UIImageView *iconView;
 @property (nonatomic,strong) UILabel *headerLabel;
 @property (nonatomic,strong) UILabel *titleLabel;
-
+/// TODO: 传入需要的模型
 @property (nonatomic, strong) GKLDHomeBlockListDetailModel *model;
 
 -(instancetype)initWithFrame:(CGRect)frame Model:(GKLDHomeBlockListDetailModel *)model;
